ba6b7c3ff84103b6f88b238add27cc1d6d5d77f0
Jake Turner <jake@unity3d.com>
2020-05-23 18:50:00 +0100

    Day1 Part1 & Part2 done

--- AoC2018/Day01/Day01Tests.cs
+++ AoC2018/Day01/Day01Tests.cs
@@ -6,10 +6,69 @@ namespace Day01
     public class Tests
     {
         [Test]
-        [TestCase("", -3)]
-        public void Day01(string directions, int expected)
+        [TestCase(new string[] {
+"+1",
+"-2",
+"+3",
+"+1"
+        }, 3, TestName = "FinalFrequency A = 3")]
+        [TestCase(new string[] {
+"+1",
+"+1",
+"+1"
+        }, 3, TestName = "FinalFrequency B = 3")]
+        [TestCase(new string[] {
+"+1",
+"+1",
+"-2"
+        }, 0, TestName = "FinalFrequency C = 0")]
+        [TestCase(new string[] {
+"-1",
+"-2",
+"-3"
+        }, -6, TestName = "FinalFrequency D = -6")]
+        public void FinalFrequency(string[] input, long expected)
         {
-            Assert.Fail();
+            Program.Parse(input);
+            Assert.That(Program.FindFinalFrequency(), Is.EqualTo(expected));
+        }
+
+        [Test]
+        [TestCase(new string[] {
+"+1",
+"-2",
+"+3",
+"+1"
+        }, 2, TestName = "RepeatFrequency A = 2")]
+        [TestCase(new string[] {
+"+1",
+"-1"
+        }, 0, TestName = "RepeatFrequency B = 0")]
+        [TestCase(new string[] {
+"+3",
+"+3",
+"+4",
+"-2",
+"-4"
+        }, 10, TestName = "RepeatFrequency C = 10")]
+        [TestCase(new string[] {
+"-6",
+"+3",
+"+8",
+"+5",
+"-6"
+        }, 5, TestName = "RepeatFrequency D = 5")]
+        [TestCase(new string[] {
+"+7",
+"+7",
+"-2",
+"-7",
+"-4"
+        }, 14, TestName = "RepeatFrequency E = 14")]
+        public void FindFirstRepeatFrequency(string[] input, long expected)
+        {
+            Program.Parse(input);
+            Assert.That(Program.FindFirstRepeatFrequency(), Is.EqualTo(expected));
         }
     }
 }
