93cb12dedbe3b49eb9b98908dc0c7db99aa1500c
Jake Turner <jake@unity3d.com>
2020-05-24 15:23:08 +0100

    Day4 Part1 & Part2 done

--- AoC2018/Day04/Day04.cs
+++ AoC2018/Day04/Day04.cs
@@ -63,12 +63,37 @@ You'll need to organize them before they can be analyzed.
 
 What is the ID of the guard you chose multiplied by the minute you chose? (In the above example, the answer would be 10 * 24 = 240.)
 
+Your puzzle answer was 99759.
+
+--- Part Two ---
+
+Strategy 2: Of all guards, which guard is most frequently asleep on the same minute?
+
+In the example aboe, Guard #99 spent minute 45 asleep more than any other guard or minute - three times in total. 
+(In all other cases, any guard spent any minute asleep at most twice.)
+
+What is the ID of the guard you chose multiplied by the minute you chose? (In the above example, the answer would be 99 * 45 = 4455.)
+
 */
 
 namespace Day04
 {
     class Program
     {
+        public enum GuardState { START_SHIFT, FALL_ASLEEP, WAKEUP };
+        const int MAX_NUM_GUARDS = 1024 * 128;
+        readonly static bool[] sValidGuard = new bool[MAX_NUM_GUARDS];
+
+        const int MAX_NUM_ENTRIES = 1024;
+        static int sEntriesCount;
+        readonly static int[] sYears = new int[MAX_NUM_ENTRIES];
+        readonly static int[] sMonths = new int[MAX_NUM_ENTRIES];
+        readonly static int[] sDays = new int[MAX_NUM_ENTRIES];
+        readonly static int[] sHours = new int[MAX_NUM_ENTRIES];
+        readonly static int[] sMinutes = new int[MAX_NUM_ENTRIES];
+        readonly static GuardState[] sGuardStates = new GuardState[MAX_NUM_ENTRIES];
+        readonly static int[] sGuardIds = new int[MAX_NUM_ENTRIES];
+
         private Program(string inputFile, bool part1)
         {
             var lines = AoC.Program.ReadLines(inputFile);
@@ -76,9 +101,9 @@ namespace Day04
 
             if (part1)
             {
-                long result1 = -666;
+                var result1 = GuardMostAsleepMinute();
                 Console.WriteLine($"Day04 : Result1 {result1}");
-                long expected = 280;
+                var expected = 99759;
                 if (result1 != expected)
                 {
                     throw new InvalidProgramException($"Part1 is broken {result1} != {expected}");
@@ -86,9 +111,9 @@ namespace Day04
             }
             else
             {
-                long result2 = -123;
+                var result2 = GuardMostAsleepPerMinute();
                 Console.WriteLine($"Day04 : Result2 {result2}");
-                long expected = 1797;
+                var expected = 97884;
                 if (result2 != expected)
                 {
                     throw new InvalidProgramException($"Part2 is broken {result2} != {expected}");
@@ -98,11 +123,317 @@ namespace Day04
 
         public static void Parse(string[] lines)
         {
+            for (var g = 0; g < MAX_NUM_GUARDS; ++g)
+            {
+                sValidGuard[g] = false;
+            }
+
+            var e = 0;
+            foreach (var line in lines)
+            {
+                //Timestamps are written using year-month-day hour:minute format.
+                //The guard falling asleep or waking up is always the one whose shift most recently started.
+                //Because all asleep/awake times are during the midnight hour (00:00 - 00:59), only the minute portion (00 - 59) is relevant for those events.
+                var tokens = line.Trim().Split(']');
+
+                //'[1518-11-01 00:55'
+                var dateTimeTokens = tokens[0].TrimStart('[').Trim().Split();
+                var dateTokens = dateTimeTokens[0].Trim().Split('-');
+                var year = int.Parse(dateTokens[0]);
+                var month = int.Parse(dateTokens[1]);
+                var day = int.Parse(dateTokens[2]);
+
+                var timeTokens = dateTimeTokens[1].Trim().Split(':');
+                var hour = int.Parse(timeTokens[0]);
+                var minute = int.Parse(timeTokens[1]);
+
+                //'wakes up'
+                //'Guard #10 begins shift'
+                //'falls asleep'
+                var eventTokens = tokens[1].Trim();
+
+                GuardState guardState;
+                int guardId = int.MinValue;
+                if (eventTokens == "wakes up")
+                {
+                    guardState = GuardState.WAKEUP;
+                }
+                else if (eventTokens == "falls asleep")
+                {
+                    guardState = GuardState.FALL_ASLEEP;
+                }
+                else if (eventTokens.StartsWith("Guard #") && eventTokens.EndsWith(" begins shift"))
+                {
+                    var guardToken = eventTokens.Split()[1].TrimStart('#').Trim();
+                    guardId = int.Parse(guardToken);
+                    guardState = GuardState.START_SHIFT;
+                    if (guardId > MAX_NUM_GUARDS)
+                    {
+                        throw new InvalidProgramException($"Guard {guardId} is too large MAX:{MAX_NUM_GUARDS}");
+                    }
+                    sValidGuard[guardId] = true;
+                }
+                else
+                {
+                    throw new InvalidProgramException($"Unknown event {eventTokens}");
+                }
+                sYears[e] = year;
+                sMonths[e] = month;
+                sDays[e] = day;
+                sHours[e] = hour;
+                sMinutes[e] = minute;
+                sGuardStates[e] = guardState;
+                sGuardIds[e] = guardId;
+                ++e;
+            }
+            sEntriesCount = e;
+
+            // Sort the entries in chronological order
+            for (var e1 = 0; e1 < sEntriesCount - 1; ++e1)
+            {
+                for (var e2 = e1 + 1; e2 < sEntriesCount; ++e2)
+                {
+                    if (sYears[e2] > sYears[e1])
+                    {
+                        continue;
+                    }
+                    if (sYears[e2] == sYears[e1])
+                    {
+                        if (sMonths[e2] > sMonths[e1])
+                        {
+                            continue;
+                        }
+                        if (sMonths[e2] == sMonths[e1])
+                        {
+                            if (sDays[e2] > sDays[e1])
+                            {
+                                continue;
+                            }
+                            if (sDays[e2] == sDays[e1])
+                            {
+                                if (sHours[e2] > sHours[e1])
+                                {
+                                    continue;
+                                }
+                                if (sHours[e2] == sHours[e1])
+                                {
+                                    if (sMinutes[e2] > sMinutes[e1])
+                                    {
+                                        continue;
+                                    }
+                                    if (sMinutes[e2] == sMinutes[e1])
+                                    {
+                                        throw new InvalidProgramException($"Identical date-times");
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    var year = sYears[e1];
+                    sYears[e1] = sYears[e2];
+                    sYears[e2] = year;
+                    var month = sMonths[e1];
+                    sMonths[e1] = sMonths[e2];
+                    sMonths[e2] = month;
+                    var day = sDays[e1];
+                    sDays[e1] = sDays[e2];
+                    sDays[e2] = day;
+                    var hour = sHours[e1];
+                    sHours[e1] = sHours[e2];
+                    sHours[e2] = hour;
+                    var minute = sMinutes[e1];
+                    sMinutes[e1] = sMinutes[e2];
+                    sMinutes[e2] = minute;
+                    var guard = sGuardIds[e1];
+                    sGuardIds[e1] = sGuardIds[e2];
+                    sGuardIds[e2] = guard;
+                    var awake = sGuardStates[e1];
+                    sGuardStates[e1] = sGuardStates[e2];
+                    sGuardStates[e2] = awake;
+                }
+            }
+
+            // Fill in the unknown guards values
+            var currentGuard = int.MinValue;
+            for (var e1 = 0; e1 < sEntriesCount; ++e1)
+            {
+                if (sGuardIds[e1] == int.MinValue)
+                {
+                    if (currentGuard == int.MinValue)
+                    {
+                        throw new InvalidProgramException($"Invalid uknown guard as the first entry");
+                    }
+                    sGuardIds[e1] = currentGuard;
+                }
+                currentGuard = sGuardIds[e1];
+            }
+        }
+
+        static void LogEntries()
+        {
+            for (var e1 = 0; e1 < sEntriesCount; ++e1)
+            {
+                Console.WriteLine($"{sYears[e1]}-{sMonths[e1]}-{sDays[e1]} {sHours[e1]}:{sMinutes[e1]} Guard {sGuardIds[e1]} State {sGuardStates[e1]}");
+            }
         }
 
         public static int GuardMostAsleepMinute()
         {
-            return int.MinValue;
+            var totalAsleepPerGuard = new int[MAX_NUM_GUARDS];
+            var asleepEventPerGuard = new int[MAX_NUM_GUARDS];
+            var awakePerGuard = new bool[MAX_NUM_GUARDS];
+            for (var g = 0; g < MAX_NUM_GUARDS; ++g)
+            {
+                totalAsleepPerGuard[g] = 0;
+                asleepEventPerGuard[g] = -1;
+                awakePerGuard[g] = true;
+            }
+            var maxAsleepGuard = -1;
+            var maxAsleepTime = int.MinValue;
+
+            for (var e = 0; e < sEntriesCount; ++e)
+            {
+                var g = sGuardIds[e];
+                var nowAwake = sGuardStates[e] == GuardState.WAKEUP;
+                var nowAsleep = sGuardStates[e] == GuardState.FALL_ASLEEP;
+                var wasAwake = awakePerGuard[g];
+                var lastEvent = asleepEventPerGuard[g];
+
+                if (sGuardStates[e] == GuardState.START_SHIFT)
+                {
+                    if (!wasAwake)
+                    {
+                        throw new InvalidProgramException($"Guard {g} ended shift asleep");
+                    }
+                }
+                if (!wasAwake && nowAwake)
+                {
+                    // add sleep time
+                    var lastHour = sHours[lastEvent];
+                    var hour = sHours[e];
+                    if (lastHour != hour)
+                    {
+                        throw new InvalidProgramException($"lastHour != hour {lastHour} != {hour}");
+                    }
+                    if (hour != 0)
+                    {
+                        throw new InvalidProgramException($"hour != 0 {hour}");
+                    }
+
+                    var lastMinute = sMinutes[lastEvent];
+                    var minute = sMinutes[e];
+                    var delta = minute - lastMinute;
+                    totalAsleepPerGuard[g] += delta;
+                    if (totalAsleepPerGuard[g] > maxAsleepTime)
+                    {
+                        maxAsleepTime = totalAsleepPerGuard[g];
+                        maxAsleepGuard = g;
+                    }
+                }
+                if (nowAsleep)
+                {
+                    asleepEventPerGuard[g] = e;
+                }
+
+                awakePerGuard[g] = !nowAsleep;
+            }
+
+            for (var g = 0; g < MAX_NUM_GUARDS; ++g)
+            {
+                if (sValidGuard[g] && !awakePerGuard[g])
+                {
+                    throw new InvalidProgramException($"Guard {g} is still asleep");
+                }
+            }
+
+            // Count per minute for the most asleep guard
+            var countAsleepPerMinute = new int[60];
+            var startAsleepMinute = -1;
+            var maxSleepsPerMinute = int.MinValue;
+            var mostAsleepMinute = -1;
+            for (var e = 0; e < sEntriesCount; ++e)
+            {
+                var g = sGuardIds[e];
+                if (g != maxAsleepGuard)
+                {
+                    continue;
+                }
+                if (sGuardStates[e] == GuardState.START_SHIFT)
+                {
+                    startAsleepMinute = -1;
+                    continue;
+                }
+                else if (sGuardStates[e] == GuardState.FALL_ASLEEP)
+                {
+                    startAsleepMinute = sMinutes[e];
+                    continue;
+                }
+                else if (sGuardStates[e] == GuardState.WAKEUP)
+                {
+                    var endAsleepMinute = sMinutes[e];
+                    for (var m = startAsleepMinute; m < endAsleepMinute; ++m)
+                    {
+                        ++countAsleepPerMinute[m];
+                        if (countAsleepPerMinute[m] > maxSleepsPerMinute)
+                        {
+                            maxSleepsPerMinute = countAsleepPerMinute[m];
+                            mostAsleepMinute = m;
+                        }
+                    }
+                    startAsleepMinute = -1;
+                }
+            }
+
+            Console.WriteLine($"Sleepiest Guard {maxAsleepGuard} Time {maxAsleepTime} Max Times {maxSleepsPerMinute} at {mostAsleepMinute}");
+            return maxAsleepGuard * mostAsleepMinute;
+        }
+
+        public static int GuardMostAsleepPerMinute()
+        {
+            var maxAsleepGuard = -1;
+            var mostAsleepMinute = -1;
+            var maxSleepsPerMinute = int.MinValue;
+
+            for (var g = 0; g < MAX_NUM_GUARDS; ++g)
+            {
+                // Count per minute for the guard
+                var countAsleepPerMinute = new int[60];
+                var startAsleepMinute = -1;
+                for (var e = 0; e < sEntriesCount; ++e)
+                {
+                    if (g != sGuardIds[e])
+                    {
+                        continue;
+                    }
+                    if (sGuardStates[e] == GuardState.START_SHIFT)
+                    {
+                        startAsleepMinute = -1;
+                        continue;
+                    }
+                    else if (sGuardStates[e] == GuardState.FALL_ASLEEP)
+                    {
+                        startAsleepMinute = sMinutes[e];
+                        continue;
+                    }
+                    else if (sGuardStates[e] == GuardState.WAKEUP)
+                    {
+                        var endAsleepMinute = sMinutes[e];
+                        for (var m = startAsleepMinute; m < endAsleepMinute; ++m)
+                        {
+                            ++countAsleepPerMinute[m];
+                            if (countAsleepPerMinute[m] > maxSleepsPerMinute)
+                            {
+                                maxSleepsPerMinute = countAsleepPerMinute[m];
+                                maxAsleepGuard = g;
+                                mostAsleepMinute = m;
+                            }
+                        }
+                        startAsleepMinute = -1;
+                    }
+                }
+            }
+
+            return maxAsleepGuard * mostAsleepMinute;
         }
 
         public static void Run()
--- AoC2018/Day04/Day04Tests.cs
+++ AoC2018/Day04/Day04Tests.cs
@@ -30,5 +30,31 @@ namespace Day04
             Program.Parse(input);
             Assert.That(Program.GuardMostAsleepMinute(), Is.EqualTo(expected));
         }
+
+        [Test]
+        [TestCase(new string[] {
+"[1518-11-03 00:29] wakes up",
+"[1518-11-01 00:55] wakes up",
+"[1518-11-01 00:05] falls asleep",
+"[1518-11-01 00:25] wakes up",
+"[1518-11-04 00:02] Guard #99 begins shift",
+"[1518-11-01 00:00] Guard #10 begins shift",
+"[1518-11-03 00:24] falls asleep",
+"[1518-11-04 00:46] wakes up",
+"[1518-11-01 23:58] Guard #99 begins shift",
+"[1518-11-01 00:30] falls asleep",
+"[1518-11-05 00:45] falls asleep",
+"[1518-11-04 00:36] falls asleep",
+"[1518-11-03 00:05] Guard #10 begins shift",
+"[1518-11-02 00:40] falls asleep",
+"[1518-11-02 00:50] wakes up",
+"[1518-11-05 00:03] Guard #99 begins shift",
+"[1518-11-05 00:55] wakes up"
+        }, 4455, TestName = "GuardMostAsleepMinute 4455")]
+        public void GuardMostAsleepPerMinute(string[] input, int expected)
+        {
+            Program.Parse(input);
+            Assert.That(Program.GuardMostAsleepPerMinute(), Is.EqualTo(expected));
+        }
     }
 }
