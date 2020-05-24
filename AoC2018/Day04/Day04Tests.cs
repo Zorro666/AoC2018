e0c6eb94d0fc231ca841dd1f8c4c2be72b20b814
Jake Turner <jake@unity3d.com>
2020-05-24 07:26:27 +0100

    Day4 Start

--- AoC2018/Day04/Day04Tests.cs
+++ AoC2018/Day04/Day04Tests.cs
@@ -6,10 +6,29 @@ namespace Day04
     public class Tests
     {
         [Test]
-        [TestCase("", -3)]
-        public void Day04(string directions, int expected)
+        [TestCase(new string[] {
+"[1518-11-03 00:29] wakes up",
+"[1518-11-01 00:55] wakes up",
+"[1518-11-01 00:00] Guard #10 begins shift",
+"[1518-11-01 00:25] wakes up",
+"[1518-11-04 00:02] Guard #99 begins shift",
+"[1518-11-03 00:24] falls asleep",
+"[1518-11-04 00:46] wakes up",
+"[1518-11-01 23:58] Guard #99 begins shift",
+"[1518-11-01 00:05] falls asleep",
+"[1518-11-01 00:30] falls asleep",
+"[1518-11-04 00:36] falls asleep",
+"[1518-11-03 00:05] Guard #10 begins shift",
+"[1518-11-02 00:40] falls asleep",
+"[1518-11-02 00:50] wakes up",
+"[1518-11-05 00:45] falls asleep",
+"[1518-11-05 00:03] Guard #99 begins shift",
+"[1518-11-05 00:55] wakes up"
+        }, 240, TestName = "GuardMostAsleepMinute 240")]
+        public void GuardMostAsleepMinute(string[] input, int expected)
         {
-            Assert.Fail();
+            Program.Parse(input);
+            Assert.That(Program.GuardMostAsleepMinute(), Is.EqualTo(expected));
         }
     }
 }
