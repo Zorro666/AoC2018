44a67c4663b0def980059f8dc3d2aec7de9d9917
Jake Turner <jake@unity3d.com>
2020-05-30 05:49:14 +0100

    Day8 Part1 done

--- AoC2018/Day08/Day08.cs
+++ AoC2018/Day08/Day08.cs
@@ -42,12 +42,53 @@ In this example, that sum is 1+1+2+10+11+12+2+99=138.
 
 What is the sum of all metadata entries?
 
+Your puzzle answer was 48155.
+
+--- Part Two ---
+
+The second check is slightly more complicated: you need to find the value of the root node (A in the example above).
+
+The value of a node depends on whether it has child nodes.
+
+If a node has no child nodes, its value is the sum of its metadata entries.
+So, the value of node B is 10+11+12=33, and the value of node D is 99.
+
+However, if a node does have child nodes, the metadata entries become indexes which refer to those child nodes.
+A metadata entry of 1 refers to the first child node, 2 to the second, 3 to the third, and so on.
+The value of this node is the sum of the values of the child nodes referenced by the metadata entries.
+If a referenced child node does not exist, that reference is skipped.
+A child node can be referenced multiple time and counts each time it is referenced.
+A metadata entry of 0 does not refer to any child node.
+
+For example, again using the above nodes:
+
+Node C has one metadata entry, 2.
+Because node C has only one child node, 2 references a child node which does not exist, and so the value of node C is 0.
+Node A has three metadata entries: 1, 1, and 2.
+The 1 references node A's first child node, B, and the 2 references node A's second child node, C.
+Because node B has a value of 33 and node C has a value of 0, the value of node A is 33+33+0=66.
+So, in this example, the value of the root node is 66.
+
+What is the value of the root node?
+
 */
 
 namespace Day08
 {
     class Program
     {
+        const int MAX_NUM_METADATA_PER_NODE = 128;
+        const int MAX_NUM_CHILD_NODES = 128;
+        const int MAX_NUM_NODES = 2048;
+        const int MAX_NUM_VALUES = MAX_NUM_NODES * MAX_NUM_METADATA_PER_NODE;
+        readonly static int[,] sMetadatas = new int[MAX_NUM_NODES, MAX_NUM_METADATA_PER_NODE];
+        readonly static int[,] sChilds = new int[MAX_NUM_NODES, MAX_NUM_CHILD_NODES];
+        readonly static int[] sChildsCount = new int[MAX_NUM_NODES];
+        readonly static int[] sMetadatasCount = new int[MAX_NUM_NODES];
+        readonly static int[] sValues = new int[MAX_NUM_VALUES];
+        static int sValuesCount;
+        static int sNodesCount;
+
         private Program(string inputFile, bool part1)
         {
             var lines = AoC.Program.ReadLines(inputFile);
@@ -57,7 +98,7 @@ namespace Day08
             {
                 var result1 = Sum();
                 Console.WriteLine($"Day08 : Result1 {result1}");
-                var expected = 280;
+                var expected = 48155;
                 if (result1 != expected)
                 {
                     throw new InvalidProgramException($"Part1 is broken {result1} != {expected}");
@@ -77,11 +118,68 @@ namespace Day08
 
         public static void Parse(string[] lines)
         {
+            if (lines.Length != 1)
+            {
+                throw new InvalidProgramException($"Bad input expected a single line got {lines.Length}");
+            }
+            var tokens = lines[0].Trim().Split();
+            sValuesCount = 0;
+            foreach (var token in tokens)
+            {
+                sValues[sValuesCount] = int.Parse(token);
+                ++sValuesCount;
+            }
+
+            sNodesCount = 0;
+            var end = ReadNode(0);
+            if (end != sValuesCount)
+            {
+                throw new InvalidProgramException($"Unexpected end after reading all nodes expected {sValuesCount} got {end}");
+            }
+        }
+
+        static int ReadNode(int start)
+        {
+            // A header, which is always exactly two numbers:
+            // The quantity of child nodes.
+            // The quantity of metadata entries.
+            // Zero or more child nodes (as specified in the header).
+            // One or more metadata entries (as specified in the header).
+            // Each child node is itself a node that has its own header, child nodes, and metadata.
+            var thisNode = sNodesCount;
+            ++sNodesCount;
+            var childNodesCount = sValues[start + 0];
+            var metadatasCount = sValues[start + 1];
+            var index = start + 2;
+
+            sChildsCount[thisNode] = childNodesCount;
+            for (var c = 0; c < childNodesCount; ++c)
+            {
+                sChilds[thisNode, c] = sNodesCount;
+                index = ReadNode(index);
+            }
+
+            sMetadatasCount[thisNode] = metadatasCount;
+            for (var m = 0; m < metadatasCount; ++m)
+            {
+                sMetadatas[thisNode, m] = sValues[index];
+                ++index;
+            }
+
+            return index;
         }
 
         public static long Sum()
         {
-            return long.MinValue;
+            var sum = 0L;
+            for (var n = 0; n < sNodesCount; ++n)
+            {
+                for (var m = 0; m < sMetadatasCount[n]; ++m)
+                {
+                    sum += sMetadatas[n, m];
+                }
+            }
+            return sum;
         }
 
         public static void Run()
