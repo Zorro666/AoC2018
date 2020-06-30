9ea70e4f3d9bd33316975adffafd878727feee44
Jake Turner <jake@unity3d.com>
2020-06-30 07:34:48 +0100

    Day17 Start

--- AoC2018/Day17/Day17Tests.cs
+++ AoC2018/Day17/Day17Tests.cs
@@ -6,10 +6,20 @@ namespace Day17
     public class Tests
     {
         [Test]
-        [TestCase("", -3)]
-        public void Day17(string directions, int expected)
+        [TestCase(new string[] {
+"x=495, y=2..7",
+"y=7, x=495..501",
+"x=501, y=3..7",
+"x=498, y=2..4",
+"x=506, y=1..2",
+"x=498, y=10..13",
+"x=504, y=10..13",
+"y=13, x=498..504"
+        }, 57, TestName = "CountWaterTiles A = 57")]
+        public void CountWaterTiles(string[] input, int expected)
         {
-            Assert.Fail();
+            Program.Parse(input);
+            Assert.That(Program.CountWaterTiles(), Is.EqualTo(expected));
         }
     }
 }
