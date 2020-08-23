7f06fa9677c0517752b4171f9adbc032bd7f1f52
Jake Turner <jake@unity3d.com>
2020-08-23 05:31:08 +0100

    Day24 Start

--- AoC2018/Day24/Day24Tests.cs
+++ AoC2018/Day24/Day24Tests.cs
@@ -6,10 +6,19 @@ namespace Day24
     public class Tests
     {
         [Test]
-        [TestCase("", -3)]
-        public void Day24(string directions, int expected)
+        [TestCase(new string[] {
+"Immune System:",
+"17 units each with 5390 hit points (weak to radiation, bludgeoning) with an attack that does 4507 fire damage at initiative 2",
+"989 units each with 1274 hit points (immune to fire; weak to bludgeoning, slashing) with an attack that does 25 slashing damage at initiative 3",
+"",
+"Infection:",
+"801 units each with 4706 hit points (weak to radiation) with an attack that does 116 bludgeoning damage at initiative 1",
+"4485 units each with 2961 hit points (immune to radiation; weak to fire, cold) with an attack that does 12 slashing damage at initiative 4"
+        }, 5126, TestName = "WinningArmyUnits A = 5126")]
+        public void WinningArmyUnits(string[] input, int expected)
         {
-            Assert.Fail();
+            Program.Parse(input);
+            Assert.That(Program.WinningArmyUnits(), Is.EqualTo(expected));
         }
     }
 }
