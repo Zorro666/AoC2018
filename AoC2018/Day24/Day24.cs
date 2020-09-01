09a69ca63f7b2bf0ef8528dda010e2018347097a
Jake Turner <jake@unity3d.com>
2020-09-01 21:51:18 +0100

    Day24 Part1 is done : start of Part2

--- AoC2018/Day24/Day24.cs
+++ AoC2018/Day24/Day24.cs
@@ -171,6 +171,146 @@ You scan the reindeer's condition (your puzzle input); the white-bearded man loo
 
 As it stands now, how many units would the winning army have?
 
+Your puzzle answer was 28976.
+
+--- Part Two ---
+
+Things aren't looking good for the reindeer.
+The man asks whether more milk and cookies would help you think.
+
+If only you could give the reindeer's immune system a boost, you might be able to change the outcome of the combat.
+
+A boost is an integer increase in immune system units' attack damage.
+For example, if you were to boost the above example's immune system's units by 1570, the armies would instead look like this:
+
+Immune System:
+17 units each with 5390 hit points (weak to radiation, bludgeoning) with
+ an attack that does 6077 fire damage at initiative 2
+989 units each with 1274 hit points (immune to fire; weak to bludgeoning,
+ slashing) with an attack that does 1595 slashing damage at initiative 3
+
+Infection:
+801 units each with 4706 hit points (weak to radiation) with an attack
+ that does 116 bludgeoning damage at initiative 1
+4485 units each with 2961 hit points (immune to radiation; weak to fire,
+ cold) with an attack that does 12 slashing damage at initiative 4
+With this boost, the combat proceeds differently:
+
+Immune System:
+Group 2 contains 989 units
+Group 1 contains 17 units
+Infection:
+Group 1 contains 801 units
+Group 2 contains 4485 units
+
+Infection group 1 would deal defending group 2 185832 damage
+Infection group 1 would deal defending group 1 185832 damage
+Infection group 2 would deal defending group 1 53820 damage
+Immune System group 2 would deal defending group 1 1577455 damage
+Immune System group 2 would deal defending group 2 1577455 damage
+Immune System group 1 would deal defending group 2 206618 damage
+
+Infection group 2 attacks defending group 1, killing 9 units
+Immune System group 2 attacks defending group 1, killing 335 units
+Immune System group 1 attacks defending group 2, killing 32 units
+Infection group 1 attacks defending group 2, killing 84 units
+Immune System:
+Group 2 contains 905 units
+Group 1 contains 8 units
+Infection:
+Group 1 contains 466 units
+Group 2 contains 4453 units
+
+Infection group 1 would deal defending group 2 108112 damage
+Infection group 1 would deal defending group 1 108112 damage
+Infection group 2 would deal defending group 1 53436 damage
+Immune System group 2 would deal defending group 1 1443475 damage
+Immune System group 2 would deal defending group 2 1443475 damage
+Immune System group 1 would deal defending group 2 97232 damage
+
+Infection group 2 attacks defending group 1, killing 8 units
+Immune System group 2 attacks defending group 1, killing 306 units
+Infection group 1 attacks defending group 2, killing 29 units
+Immune System:
+Group 2 contains 876 units
+Infection:
+Group 2 contains 4453 units
+Group 1 contains 160 units
+
+Infection group 2 would deal defending group 2 106872 damage
+Immune System group 2 would deal defending group 2 1397220 damage
+Immune System group 2 would deal defending group 1 1397220 damage
+
+Infection group 2 attacks defending group 2, killing 83 units
+Immune System group 2 attacks defending group 2, killing 427 units
+After a few fights...
+
+Immune System:
+Group 2 contains 64 units
+Infection:
+Group 2 contains 214 units
+Group 1 contains 19 units
+
+Infection group 2 would deal defending group 2 5136 damage
+Immune System group 2 would deal defending group 2 102080 damage
+Immune System group 2 would deal defending group 1 102080 damage
+
+Infection group 2 attacks defending group 2, killing 4 units
+Immune System group 2 attacks defending group 2, killing 32 units
+Immune System:
+Group 2 contains 60 units
+Infection:
+Group 1 contains 19 units
+Group 2 contains 182 units
+
+Infection group 1 would deal defending group 2 4408 damage
+Immune System group 2 would deal defending group 1 95700 damage
+Immune System group 2 would deal defending group 2 95700 damage
+
+Immune System group 2 attacks defending group 1, killing 19 units
+Immune System:
+Group 2 contains 60 units
+Infection:
+Group 2 contains 182 units
+
+Infection group 2 would deal defending group 2 4368 damage
+Immune System group 2 would deal defending group 2 95700 damage
+
+Infection group 2 attacks defending group 2, killing 3 units
+Immune System group 2 attacks defending group 2, killing 30 units
+After a few more fights...
+
+Immune System:
+Group 2 contains 51 units
+Infection:
+Group 2 contains 40 units
+
+Infection group 2 would deal defending group 2 960 damage
+Immune System group 2 would deal defending group 2 81345 damage
+
+Infection group 2 attacks defending group 2, killing 0 units
+Immune System group 2 attacks defending group 2, killing 27 units
+Immune System:
+Group 2 contains 51 units
+Infection:
+Group 2 contains 13 units
+
+Infection group 2 would deal defending group 2 312 damage
+Immune System group 2 would deal defending group 2 81345 damage
+
+Infection group 2 attacks defending group 2, killing 0 units
+Immune System group 2 attacks defending group 2, killing 13 units
+Immune System:
+Group 2 contains 51 units
+Infection:
+No groups remain.
+
+This boost would allow the immune system's armies to win! It would be left with 51 units.
+
+You don't even know how you could boost the reindeer's immune system or what effect it might have, so you need to be cautious and find the smallest boost that would allow the immune system to win.
+
+How many units does the immune system have left after getting the smallest boost it needs to win?
+
 */
 
 namespace Day24
@@ -202,8 +342,9 @@ namespace Day24
             {
                 var result1 = WinningArmyUnits();
                 Console.WriteLine($"Day24 : Result1 {result1}");
+                // 28781 : TOO LOW
                 // 29150 : TOO HIGH
-                var expected = 29150;
+                var expected = 28976;
                 if (result1 != expected)
                 {
                     throw new InvalidProgramException($"Part1 is broken {result1} != {expected}");
@@ -499,10 +640,8 @@ namespace Day24
                 }
             }
 
-            var attackerMaxDamage = new int[groupCount];
             for (var a = 0; a < groupCount; ++a)
             {
-                attackerMaxDamage[a] = int.MinValue;
                 if (sUnitCount[a] == 0)
                 {
                     continue;
@@ -520,11 +659,10 @@ namespace Day24
                     }
                     var damageAmount = ComputeDamage(a, d);
                     damage[a, d] = damageAmount;
-                    attackerMaxDamage[a] = Math.Max(damageAmount, attackerMaxDamage[a]);
                 }
             }
 
-            // sort attacker by damage then attacker initiative
+            // sort attacker by effective power then attacker initiative
             var attackTarget = new int[groupCount];
             var attackerOrder = new int[groupCount];
             for (var g = 0; g < groupCount; ++g)
@@ -539,14 +677,14 @@ namespace Day24
                 {
                     var attacker1 = attackerOrder[i];
                     var attacker2 = attackerOrder[j];
-                    var maxDamage1 = attackerMaxDamage[attacker1];
-                    var maxDamage2 = attackerMaxDamage[attacker2];
+                    var effectivePower1 = sAttackPower[attacker1] * sUnitCount[attacker1];
+                    var effectivePower2 = sAttackPower[attacker2] * sUnitCount[attacker2];
                     var swap = false;
-                    if (maxDamage2 > maxDamage1)
+                    if (effectivePower2 > effectivePower1)
                     {
                         swap = true;
                     }
-                    else if (maxDamage2 == maxDamage1)
+                    else if (effectivePower2 == effectivePower1)
                     {
                         swap = sInitiative[attacker2] > sInitiative[attacker1];
                     }
@@ -590,11 +728,13 @@ namespace Day24
                     }
                     if (dmg == maxDamage)
                     {
-                        if (attackerMaxDamage[d] > attackerMaxDamage[target])
+                        var effectivePower1 = sAttackPower[target] * sUnitCount[target];
+                        var effectivePower2 = sAttackPower[d] * sUnitCount[d];
+                        if (effectivePower2 > effectivePower1)
                         {
                             target = d;
                         }
-                        else if (attackerMaxDamage[d] == attackerMaxDamage[target])
+                        else if (effectivePower2 == effectivePower1)
                         {
                             if (sInitiative[d] > sInitiative[target])
                             {
