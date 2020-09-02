98623514cc3b544405795b26d07d756646058bf6
Jake Turner <jake@unity3d.com>
2020-09-02 22:00:58 +0100

    Day25 Part1 start

--- AoC2018/Day25/Day25Tests.cs
+++ AoC2018/Day25/Day25Tests.cs
@@ -6,10 +6,56 @@ namespace Day25
     public class Tests
     {
         [Test]
-        [TestCase("", -3)]
-        public void Day25(string directions, int expected)
+        [TestCase(new string[] {
+"0,0,0,0",
+"3,0,0,0",
+"0,3,0,0",
+"0,0,3,0",
+"0,0,0,3",
+"0,0,0,6",
+"9,0,0,0",
+"12,0,0,0"
+        }, 2, TestName = "CountConstellations A = 2")]
+        [TestCase(new string[] {
+"-1,2,2,0",
+"0,0,2,-2",
+"0,0,0,-2",
+"-1,2,0,0",
+"-2,-2,-2,2",
+"3,0,2,-1",
+"-1,3,2,2",
+"-1,0,-1,0",
+"0,2,1,-2",
+"3,0,0,0"
+        }, 4, TestName = "CountConstellations B = 4")]
+        [TestCase(new string[] {
+"1,-1,0,1",
+"2,0,-1,0",
+"3,2,-1,0",
+"0,0,3,1",
+"0,0,-1,-1",
+"2,3,-2,0",
+"-2,2,0,0",
+"2,-2,0,-1",
+"1,-1,0,-1",
+"3,2,0,2"
+        }, 3, TestName = "CountConstellations C = 3")]
+        [TestCase(new string[] {
+"1,-1,-1,-2",
+"-2,-2,0,1",
+"0,2,1,3",
+"-2,3,-2,1",
+"0,2,3,-2",
+"-1,-1,1,-2",
+"0,-2,-1,0",
+"-2,2,3,-1",
+"1,2,2,0",
+"-1,-2,0,-2"
+        }, 8, TestName = "CountConstellations D = 8")]
+        public void CountConstellations(string[] input, int expected)
         {
-            Assert.Fail();
+            Program.Parse(input);
+            Assert.That(Program.CountConstellations(), Is.EqualTo(expected));
         }
     }
 }
